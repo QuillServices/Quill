459abd109e69496a9333b48f556e6c77d1b84da8
Ian Brady <ianbrady3240@outlook.com>
2026-06-26 13:33:17 -0500

    Add PWA support

--- src/index.js
+++ src/index.js
@@ -15,3 +15,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => console.log('Quill SW registered'))
+      .catch(err => console.log('SW error:', err));
+  });
+}
